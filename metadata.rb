f2f85405d923d28856493b5151c8573e4f93af48
Alex Falkowski <alexrfalkowski@gmail.com>
2014-10-25 16:23:35 +1100

    Created with berks cookbook chef-teamcity.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'chef-teamcity'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef-teamcity'
+long_description 'Installs/Configures chef-teamcity'
+version          '0.1.0'
+
