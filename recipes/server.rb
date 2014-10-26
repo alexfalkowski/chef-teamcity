668f2c729b488bb159200413d9049e0ec9fa547c
Alex Falkowski <alexrfalkowski@gmail.com>
2014-10-26 15:12:46 +1100

    Added the ability to extract TeamCity.

--- recipes/server.rb
+++ recipes/server.rb
@@ -7,18 +7,32 @@
 # All rights reserved - Do Not Redistribute
 #
 
-group 'teamcity'
+VERSION = '8.1.5'
+USER_NAME = 'teamcity'
+SRC_PATH = "/opt/TeamCity-#{VERSION}.tar.gz".freeze
+EXTRACT_PATH = "/opt/TeamCity-#{VERSION}".freeze
 
-user 'teamcity' do
-  comment 'TeamCity user.'
-  gid 'teamcity'
+group USER_NAME
+
+user USER_NAME do
+  gid USER_NAME
   shell '/bin/bash'
   password '$1$ByY03mDX$4pk9wp9bC19yB6pxSoVB81'
 end
 
-remote_file 'opt/TeamCity-8.1.5.tar.gz' do
-  source 'http://download.jetbrains.com/teamcity/TeamCity-8.1.5.tar.gz'
-  owner 'teamcity'
-  group 'teamcity'
+remote_file SRC_PATH do
+  source "http://download.jetbrains.com/teamcity/TeamCity-#{VERSION}.tar.gz"
+  owner USER_NAME
+  group USER_NAME
   mode '0644'
+end
+
+bash 'extract_teamcity' do
+  cwd '/opt'
+  code <<-EOH
+    mkdir -p #{EXTRACT_PATH}
+    tar xzf #{SRC_PATH} -C #{EXTRACT_PATH}
+    mv #{EXTRACT_PATH}/*/* #{EXTRACT_PATH}/
+  EOH
+  not_if { ::File.exists?(EXTRACT_PATH) }
 end
