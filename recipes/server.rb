bcd218da1cada1c126e660cd09277d3590a45075
Alex Falkowski <alexrfalkowski@gmail.com>
2014-10-26 14:45:50 +1100

    Added user and group.

--- recipes/server.rb
+++ recipes/server.rb
@@ -6,3 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+group 'teamcity'
+
+user 'teamcity' do
+  comment 'TeamCity user.'
+  gid 'teamcity'
+  shell '/bin/bash'
+  password '$1$ByY03mDX$4pk9wp9bC19yB6pxSoVB81'
+end
