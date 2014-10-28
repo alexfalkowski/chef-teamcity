6c58156f03b62349dc6d78961bda1374f9092a3f
Alex Falkowski <alex.falkowski@mq.edu.au>
2014-10-28 16:24:00 +1100

    Added dependencies to the metadata.

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,13 @@ maintainer 'Alex Falkowski'
 maintainer_email 'alexrfalkowski@gmail.com'
 license 'All rights reserved'
 description 'Installs/Configures TeamCity agent/server.'
-long_description 'Installs/Configures TeamCity agent/server.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
+recipe 'chef-teamcity::server', 'Install a TeamCity server'
+recipe 'chef-teamcity::agent', 'Installs a TeamCity agent'
+
+%w{centos}.each do |el|
+  supports el, '~> 6.0'
+end
+
+depends 'java'
